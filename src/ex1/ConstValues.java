60f45b17e610b836954b8a0e3b72ff19b37f74b2
MAThass <mateuszformela0504@gmail.com>
2025-11-22 17:28:19 +0100

    add 1 example to report fle

--- src/ex1/ConstValues.java
+++ src/ex1/ConstValues.java
@@ -3,8 +3,8 @@ package ex1;
 public class ConstValues {
     public static final double g_const = 9.81;
     public static final int numberOfRecord = 100000;
-    public static final int blockingFactor = 10;
-    public static final int numberOfBuffers = 1001;
+    public static final int blockingFactor = 100;
+    public static final int numberOfBuffers = 100;
     public static final int blockSize = blockingFactor*12;
     public static final int minRangeRecord = 1;
     public static final int maxRangeRecord = 99;
